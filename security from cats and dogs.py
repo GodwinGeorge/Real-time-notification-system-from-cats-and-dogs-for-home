8ee22109f874e190a1a7637404e8bd6c15b8ae87
Godwin George <49614978+GodwinGeorge@users.noreply.github.com>
2021-08-12 12:48:06 +0530

    Add files via upload
    Provided the code in ipynb and py formats. Enjoy your day!!

---
+++ security from cats and dogs.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+import requests
+import io
+import json
+import matplotlib.pyplot as plt
+import pyttsx3
+import time
+
+
+# In[2]:
+
+
+engine = pyttsx3.init()
+
+
+# In[ ]:
+
+
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+starting_time = time.time()
+frame_id = 0
+while True:
+    _, img = cap.read()
+    frame_id +=1
+    height, width, channels = img.shape
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+                indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+                for i in range(len(boxes)):
+                    if i in indexes:
+                        x, y, w, h = boxes[i]
+                        label = str(classes[class_ids[i]])
+                        color = colors[class_ids[i]]
+                        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                        cv2.putText(img, label, (x, y + 30), font, 3, color, 3)
+                        if label == "cat":
+                            engine.say(label)
+                            engine.say("Entered the room")
+                        if label == "dog":
+                            engine.say(label)
+                            engine.say("Entered the room")
+                    elapsed_time = time.time() - starting_time
+                    fps = frame_id / elapsed_time
+                    cv2.putText(img, "FPS:" + str(fps), (10,50),font, 4, (0, 0, 0), 3)
+                    cv2.imshow("Image", img)
+                    key = cv2.waitKey(1)
+                    engine.runAndWait()
+                    if key == 27:
+                        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
